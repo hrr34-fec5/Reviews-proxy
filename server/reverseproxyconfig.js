43a7b64e97f28f9109307ef3956a46263c05a43e
Vincent L <liberty.sky87@gmail.com>
2018-11-06 14:55:19 -0800

    proxy setup

---
+++ server/reverseproxyconfig.js
@@ -0,0 +1,20 @@
+{
+  "routes": [
+    {
+      "route": "/vince",
+      "address": "http://localhost:1337"
+    },
+    {
+      "route": "/sean",
+      "address": "http://localhost:3001"
+    },
+    {
+      "route": "/pat",
+      "address": "http://localhost:8080"
+    },
+    {
+      "route": "/stephen",
+      "address": "http://localhost:3030"
+    },
+  ]
+}
