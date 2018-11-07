e7f8b831715707363879afa836377de9e7bfcca9
Vincent L <liberty.sky87@gmail.com>
2018-11-07 11:33:04 -0800

    update proxy

--- server/reverseproxy.js
+++ server/reverseproxy.js
@@ -1,24 +1,40 @@
 const express = require('express');
-const morgan = require ('morgan');
+const httpProxy = require('http-proxy');
 const proxy = require('http-proxy-middleware');
 const path = require('path');
+
+const apiProxy = httpProxy.createProxyServer();
 const port = process.env.PORT || 3000;
 const app = express();
 
-const { routes } = require('./reverseproxyconfig.json');
+const vince =   'http://localhost:1337',
+      sean =    'http://localhost:3001',
+      pat =     'http://localhost:8080',
+      stephen = 'http://localhost:3030';
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-for (route of routes) {
-    app.use(route.route,
-        proxy({
-            target: route.address,
-            pathRewrite: (path, req) => path.split('/').slice(2).join('/')
-        })
-    );
-}
+// app.all('/app1/*', (req, res) => {
+//     console.log('redirecting to reviews');
+//     apiProxy.web(req, res, {target: vince});
+// });
 
-app.listen(port, () => {
-    console.log(`Proxy is listening on port ${port}`);
-});
+// app.all('/app2/*', (req, res) => {
+//     console.log('redirecting to calendar');
+//     apiProxy.web(req, res, {target: stephen});
+// });
+
+// app.all('/app3/*', (req, res) => {
+//     console.log('redirecting to photostream');
+//     apiProxy.web(req, res, {target: pat});
+// });
+
+// app.all('/app4/*', (req, res) => {
+//     console.log('redirecting to info');
+//     apiProxy.web(req, res, {target: sean});
+// });
+
+app.get('/listing/:id', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
+
+app.listen(port, () => console.log(`Proxy is listening on port ${port}`));
